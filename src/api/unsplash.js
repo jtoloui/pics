a43ef9af40445e30529948b679171b9a2f61225e
Jamie Toloui <jamie@toloui.com>
2019-03-16 19:00:10 +0000

    corrections

---
+++ src/api/unsplash.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.unsplash.com",
+
+  headers: {
+    Authorization:
+      "Client-ID f5816073876ea168ad0f2c30c45c395f24f8793d7a39604710b315c839f333a0"
+  }
+});
