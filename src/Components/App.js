930cd796544dc5895a928e2d01134b5ddf2fd011
Jamie T <jamie@toloui.com>
2020-05-30 19:19:13 +0100

    handle images not found or api error

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,51 +1,78 @@
 import React, { Suspense } from "react";
 import Unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
-import LineBreak from './LineBreak';
+import LineBreak from "./LineBreak";
 const ImageList = React.lazy(() => import("./ImageList"));
 
 class App extends React.Component {
-  state = { images: [], error: null };
-  onSearchSubmit = async term => {
-    try {
-      const response = await Unsplash.get("/search/photos", {
-        params: {
-          query: term,
-          per_page: 20
-        }
-      });
-      this.setState({ images: response.data.results, error: null });
-    } catch (error) {
-      this.setState({ error, images: [] })
-    }
+	state = {
+		images: [],
+		error: null,
+		noImagesReturned: false,
+		lastSearchTerm: "",
+	};
+	onSearchSubmit = async (term) => {
+		try {
+			const response = await Unsplash.get("/search/photos", {
+				params: {
+					query: term,
+					per_page: 20,
+				},
+			});
+			this.setState({ lastSearchTerm: term });
+			if (response.data.total > 0) {
+				this.setState({
+					images: response.data.results,
+					error: null,
+					noImagesReturned: false,
+				});
+			} else {
+				this.setState({
+					images: [],
+					error: null,
+					noImagesReturned: true,
+				});
+			}
+		} catch (error) {
+			this.setState({ lastSearchTerm: term });
+			this.setState({ error, images: [] });
+		}
+	};
+	render() {
+		console.log(2, this.state.images);
 
-  };
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        <Suspense
-          fallback={
-            <div className="ui active dimmer">
-              <div className="ui big text loader">Loading...</div>
-            </div>
-          }
-        >
-          <SearchBar onSubmit={this.onSearchSubmit} />
-          <LineBreak class="bx--col-xs-11 bx--col-sm-11 bx--col-md-11 bx--col-lg-11" />
-
-          <ImageList images={this.state.images} />
-          {this.state.error && (
-            <div class="ui error message">
-              <div class="header">
-                Whoops
-                </div>
-              <p>Looks like we are experiencing some issues with your search. Please try again later.</p>
-            </div>
-          )}
-        </Suspense>
-      </div>
-    );
-  }
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<Suspense
+					fallback={
+						<div className="ui active dimmer">
+							<div className="ui big text loader">Loading...</div>
+						</div>
+					}
+				>
+					<SearchBar onSubmit={this.onSearchSubmit} />
+					<LineBreak class="bx--col-xs-11 bx--col-sm-11 bx--col-md-11 bx--col-lg-11" />
+					{this.state.error && (
+						<div class="ui error message">
+							<div class="header">Whoops</div>
+							<p>
+								{`Looks like we are experiencing some issues with
+								your search for "${this.state.lastSearchTerm}". Please try again later.`}
+							</p>
+						</div>
+					)}
+					{this.state.noImagesReturned ? (
+						<div className="ui warning message">
+							<div class="header">Whoops</div>
+							<p>{`Looks like we could find any images for "${this.state.lastSearchTerm}". Please search for another image.`}</p>
+						</div>
+					) : (
+						<ImageList images={this.state.images} />
+					)}
+				</Suspense>
+			</div>
+		);
+	}
 }
 
 export default App;
